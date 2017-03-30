d50e923f24e2497be6f454f0522613f283ecbb6a
rudyleiva93 <rudyleiva93@yahoo.com>
2017-03-30 10:20:48 -0700

    Created Calendar package

---
+++ Project1/src/Calendar/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calendar;
+
+/**
+ *
+ * @author rleiva
+ */
+public class Main {
+    
+    /*https://www.tutorialspoint.com/java/util/arrays_aslist.htm
+     *http://stackoverflow.com/questions/23160832/how-to-find-index-of-string-array-in-java-from-a-given-value 
+     *      -> Arrays.asList(arrayName).indexOf("itemInArray"); to return index position of an item in an array
+     *http://helpdesk.objects.com.au/java/zellers-congruence-in-java
+    */
+    
+}
